7ff7a5c2f2c379a8dd5168f76f272e7f34f7c4f9
Sandeep Dilip <34922961+sanman1k98@users.noreply.github.com>
2025-07-15 08:49:25 -0400

    chore: add more ESLint customizations

--- eslint.config.js
+++ eslint.config.js
@@ -4,6 +4,18 @@ export default antfu({
 	stylistic: {
 		indent: 'tab',
 		semi: true,
+		quotes: 'single',
+		overrides: {
+			'style/brace-style': [
+				'error',
+				'1tbs',
+				{ allowSingleLine: true },
+			],
+			'style/newline-per-chained-call': [
+				'error',
+				{ ignoreChainWithDepth: 2 },
+			],
+		},
 	},
 	formatters: true,
 	astro: true,
