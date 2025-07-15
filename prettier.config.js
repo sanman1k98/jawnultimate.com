60ffbb8057ef93997c820b9f93a4f9d5c1e79f96
Sandeep Dilip <34922961+sanman1k98@users.noreply.github.com>
2025-07-15 08:54:30 -0400

    build: setup Prettier
    Added `prettier-plugin-tailwind` to automatically sort Tailwind classes
    based on recommended class order.
    
    Also added `prettier-plugin-astro` to format Astro files.
    
    Might have to update the Prettier and ESLint configs since they might
    conflict with each other and I forgot how to set this stuff up
    properly...

---
+++ prettier.config.js
@@ -0,0 +1,30 @@
+/** @type {import("prettier").Config} */
+export default {
+	printWidth: 100,
+	semi: true,
+	singleQuote: true,
+	tabWidth: 2,
+	trailingComma: 'all',
+	useTabs: true,
+	plugins: [
+		'prettier-plugin-astro',
+		'prettier-plugin-tailwind',
+	],
+	overrides: [
+		{
+			files: ['.*', '*.md', '*.toml', '*.yml'],
+			options: {
+				useTabs: false,
+			},
+		},
+		{
+			files: ['**/*.astro'],
+			options: {
+				// Use ESLint to lint and format frontmatter.
+				astroSkipFrontmatter: true,
+				astroAllowShorthand: true,
+				parser: 'astro',
+			},
+		},
+	],
+};
