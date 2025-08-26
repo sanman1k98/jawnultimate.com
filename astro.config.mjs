677b8655c95348d3e2fb8747ace69f1bdadffa0b
Sandeep Dilip <34922961+sanman1k98@users.noreply.github.com>
2025-08-26 13:41:35 -0400

    chore: access dev server with local hostnames

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,10 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
 	vite: {
+		server: {
+			// Access dev server with local hostname when running `pnpm run dev --host`.
+			allowedHosts: ['.local'],
+		},
 		plugins: [tailwindcss()],
 	},
 	experimental: {
