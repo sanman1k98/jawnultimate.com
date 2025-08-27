facc567913a23fb7ffc4376abae656fd9ce99c8d
Sandeep Dilip <34922961+sanman1k98@users.noreply.github.com>
2025-08-27 13:31:23 -0400

    build(deps): add sitemap integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
+import sitemap from '@astrojs/sitemap';
 import tailwindcss from '@tailwindcss/vite';
-
-// @ts-check
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
+	integrations: [sitemap()],
+
 	vite: {
 		server: {
 			// Access dev server with local hostname when running `pnpm run dev --host`.
