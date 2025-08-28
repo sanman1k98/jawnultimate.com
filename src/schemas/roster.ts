287c9a0771513c06408d4d8045fa36f6169a17d7
Sandeep Dilip <34922961+sanman1k98@users.noreply.github.com>
2025-08-28 09:36:44 -0400

    impr(schema): custom validation
    - check for duplicate jersey numbers
    - list players with duplicate numbers in error message
    - enforce date-only form "YYYY-MM"
    - custom error message for date

--- src/schemas/roster.ts
+++ src/schemas/roster.ts
@@ -1,17 +1,64 @@
 import { z } from 'astro/zod';
 
+/** Date only form "YYYY-MM". */
+const rosterDateRegex = /^\d{4}-\d{2}/;
+
+const RosterDateSchema = z.coerce.string().transform((arg, ctx) => {
+	if (!rosterDateRegex.test(arg)) {
+		ctx.addIssue({
+			code: z.ZodIssueCode.custom,
+			message: 'Must use form YYYY-MM',
+		});
+	}
+
+	const date = new Date(arg);
+
+	if (Number.isNaN(date.valueOf())) {
+		ctx.addIssue({
+			code: z.ZodIssueCode.invalid_date,
+			message: 'Invalid date; must use form YYYY-MM',
+		});
+		return z.NEVER;
+	} else {
+		return date;
+	}
+});
+
+const JerseyNumberSchema = z.union([
+	z.number().min(0).max(99).int(),
+	z.string().regex(/^\d{2}$/), // Handles cases like "00".
+]);
+
 const PlayerSchema = z.object({
 	name: z.string(),
-	number: z.union([
-		z.number().min(0).max(99).int(),
-		z.string().regex(/^\d{2}$/), // Handles cases like "00".
-	]),
-	captain: z.literal(true).optional(),
+	number: JerseyNumberSchema,
+	captain: z.boolean().optional(),
+});
+
+const PlayerListSchema = PlayerSchema.array().superRefine((players, ctx) => {
+	const reverseLookup = new Map<number, string[]>();
+
+	for (const { name, number } of players) {
+		const key = Number(number);
+		if (!reverseLookup.has(key))
+			reverseLookup.set(key, [name]);
+		else
+			reverseLookup.get(key)!.push(name);
+	}
+
+	for (const [number, names] of reverseLookup.entries()) {
+		if (names.length > 1) {
+			ctx.addIssue({
+				code: z.ZodIssueCode.custom,
+				message: `Duplicate jersey number: ${names.join(', ')} have the same jersey number ${number}`,
+			});
+		}
+	}
 });
 
 export const RosterSchema = z.object({
 	title: z.string(),
 	description: z.string().optional(),
-	date: z.coerce.date(),
-	players: PlayerSchema.array(),
+	date: RosterDateSchema,
+	players: PlayerListSchema,
 });
