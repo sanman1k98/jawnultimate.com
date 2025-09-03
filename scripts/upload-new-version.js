ac5e7a9fb2553039da4bc858d368133c56730887
Sandeep Dilip <34922961+sanman1k98@users.noreply.github.com>
2025-09-03 09:56:06 -0400

    impr(scripts): `--dry-run` option for upload

--- scripts/upload-new-version.js
+++ scripts/upload-new-version.js
@@ -1,11 +1,18 @@
 import { exit } from 'node:process';
+import { parseArgs } from 'node:util';
 import { getNextCalver } from './calver-utils.js';
 import { checkSyncStatus, createGitTag, getCurrentBranch, getGitTags, isWorkingTreeClean } from './git-utils.js';
 import { run } from './proc-utils.js';
 
 const UPLOAD_BRANCH = 'main';
 
-async function upload() {
+/**
+ * Determine the next CalVer identifier, create a new Git tag for it, upload a
+ * new version to Cloudflare, and push the new tag.
+ * @param {object} opts
+ * @param {boolean} opts.dryRun Don't actually create a new tag or upload to Cloudflare.
+ */
+async function upload(opts) {
 	try {
 		console.log('Checking if current branch is main...');
 		if (await getCurrentBranch() !== UPLOAD_BRANCH) {
@@ -14,16 +21,21 @@ async function upload() {
 
 		console.log('Checking if working tree is clean...');
 		const clean = await isWorkingTreeClean();
-		if (!clean) {
+		if (opts.dryRun && !clean) {
+			console.warn('[Dry run] Working tree is not clean.');
+		} else if (!clean) {
 			throw new Error('Working tree is not clean. Commit or stash your changes before uploading a new version.');
 		}
 
 		console.log('Checking if local branch is in sync with remote...');
 		const synced = await checkSyncStatus(UPLOAD_BRANCH);
-		if (!synced) {
+		if (opts.dryRun && !synced) {
+			console.warn('[Dry run] Local branch not in sync with remote.');
+		} else if (!synced) {
 			throw new Error('Local branch is not in sync with remote. Pull remote changes and/or push local changes before uploading a new version');
 		}
 
+		console.log('Determining next version...');
 		const existingVersions = await getGitTags().then(tags =>
 			tags
 				.filter(tag => tag.startsWith('v'))
@@ -32,6 +44,11 @@ async function upload() {
 
 		const nextVersion = getNextCalver(existingVersions);
 		console.log('New version: %s', nextVersion);
+
+		if (opts.dryRun) {
+			console.log('[Dry run] Returning early.');
+			return;
+		}
 
 		const tagName = `v${nextVersion}`;
 		const message = `chore: release version ${nextVersion}`;
@@ -52,4 +69,15 @@ async function upload() {
 	}
 }
 
-upload();
+async function main() {
+	const options = {
+		'dry-run': { type: 'boolean' },
+	};
+
+	const { values } = parseArgs({ options });
+	const opts = { dryRun: values['dry-run'] };
+
+	upload(opts);
+}
+
+main();
