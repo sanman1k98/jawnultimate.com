3bd88ea2e7deff4871fcfb86a7671b432df39f0a
Sandeep Dilip <34922961+sanman1k98@users.noreply.github.com>
2025-09-06 22:47:38 -0400

    fix(scripts): handle spawn errors
    Also accept `options` param to override the options passed to `spawn`.

--- scripts/utils/proc.ts
+++ scripts/utils/proc.ts
@@ -1,20 +1,30 @@
+import type { SpawnOptions } from 'node:child_process';
 import { execFile, spawn } from 'node:child_process';
 import { promisify } from 'node:util';
 
 /** Promisified wrapper around `child_process.execFile`. */
 export const exec = promisify(execFile);
 
-/** Run `command` with live output. */
-export async function run(command: string, args: string[] = []): Promise<void> {
+/**
+ * Run a command with live input and output.
+ * @param command - Command to run.
+ * @param args - List of arguments.
+ * @param options - Override opts passed to `spawn`.
+ * @param options.stdio - Defaults to 'inherit' (for live output and input).
+ */
+export async function run(
+	command: string,
+	args: string[] = [],
+	options: SpawnOptions = { stdio: 'inherit' },
+): Promise<void> {
 	return new Promise((resolve, reject) => {
-		const proc = spawn(command, args, { stdio: 'inherit' });
-
-		proc.on('close', (code) => {
-			if (code === 0) {
-				resolve();
-			} else {
-				reject(new Error(`${command} exited with code ${code}`));
-			}
-		});
+		if (!('stdio' in options))
+			options.stdio = 'inherit';
+		spawn(command, args, options)
+			.on('error', err => reject(new Error('Command failed to spawn', { cause: err })))
+			.on('close', code =>
+				code
+					? reject(new Error('Command exited non-zero', { cause: { code } }))
+					: resolve());
 	});
 }
